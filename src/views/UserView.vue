97408a91e155ee2eb73cc140f3d620f38135cd9d
jroberto-inv <jroberto@inv.com.vc>
2022-05-10 21:12:33 -0300

    register components VUserSearchForm/VUserProfile and passing props for component VUserProfile

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    Hello World
+    <VUserSearchForm />
+    <VUserProfile :user="user" />
   </div>
 </template>
 
 <script>
+import VUserSearchForm from '@/components/VUserSearchForm'
+import VUserProfile from '@/components/VUserProfile'
 
 export default {
-  name: 'UserView'
+  name: 'UserView',
+  components: {
+    VUserSearchForm,
+    VUserProfile
+  },
+  data () {
+    return {
+      user: { name: '' }
+    }
+  }
 }
 
 </script>
